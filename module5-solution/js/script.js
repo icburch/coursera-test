70f94857b34558a37b82759cb137863c5b9a46c8
Ian Burch <ianburch@Ians-iMac.local>
2019-06-10 15:30:24 -0400

    Don't hurt me again

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -115,7 +115,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       var final = "'";
-      final += categories[chosenCategoryShortName].short_name;
+      final += chosenCategoryShortName.short_name;
       final += "'";
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", final);
 
